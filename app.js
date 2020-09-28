2d62288b5dc1bc053bed36c7eadda3898aded44b
Jamie Sinclair <jrsinclair_28@hotmail.com>
2020-09-29 02:08:03 +0800

    add variables

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-function createCityList(citySearchList) {
-    $("#city-list").empty();
+//retrieves HTML 
+function initPage() {
+    var inputEl = document.getElementById("city-input");
+    var searchEl = document.getElementById("search-button");
+    var clearEl = document.getElementById("clear-history");
+    var nameEl = document.getElementById("city-name");
+    var nameEl = document.getElementById("city-input");
+    var currentPicEl = document.getElementById("current-pic");
+    var currentTempEl = document.getElementById("temperature");
+    var currentHumidityEl = document.getElementById("humidity");
+    var currentWindEl = document.getElementById("wind-speed");
+    var curentUVEl = document.getElementById("UV-index");
+    var historyEl = document.getElementById("history")
+        //checks if item already exists in local storage, if not gives an empty string
+    var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+    console.log(searchHistory);
+
+    //personal API key for Open Weather API
+    var APIkey = "";
+
+}

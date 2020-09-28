860bb38b87628925bce6c1290b73bedd5c9cc2a4
Jamie Sinclair <jrsinclair_28@hotmail.com>
2020-09-29 04:12:59 +0800

    use DOM to add weather descriptions in JS

--- app.js
+++ app.js
@@ -18,4 +18,27 @@ function initPage() {
     //personal API key for Open Weather API
     var APIkey = "5d24139cd3087b3e091640cbcdec7262";
 
+    //when search button is clicked then read the text input by user
+    function getWeather(cityName) {
+        //get request from Open Weather Map API
+        //axiosis a promise based HTTP client thsat sports and easy to use API
+        let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey;
+        axios.get(queryURL)
+            .then(function(response) {
+                var currentDate = new Date(response.data.dt * 1000);
+                console.log(currentDate);
+                var day = currentDate.getDate();
+                var month = currentDate.getMonth();
+                var year = currentDate.getFullYear();
+                //uses the DOM to input weather icons and description, temp, humidity and windspeed
+                nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ")";
+                var weatherPic = response.data.weather[0].icon;
+                currentPicEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+                currentPicEl.setAttribute("alt", response.data.weather[0].description);
+                currentTempEl.innerHTML = "Temperature:" + k2f(response.data.main.temp) + '&#176C';
+                currentHumidityEl.innerHTML = "Humidity:" + response.data.main.humidity + '%';
+                currentWindEl.innerHTML - "Wind Speed:" + response.data.main.wind.speed + "KMPH";
+
+            })
+    }
 }
